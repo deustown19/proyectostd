508995b0fb258b6784d4a09c128ace79adc357d8
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-02-24 16:21:03 +0000

    Añadida clase Server (en principio no hay que tocar nada más de aquí)

---
+++ trunk/STD/src/server/CarServer.java
@@ -0,0 +1,54 @@
+package server;
+
+import java.net.ServerSocket;
+
+import util.SocketManager;
+
+public class CarServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		{
+			// Set the port number.
+			int port;
+			if (args.length>0){
+				try{
+					port = Integer.parseInt(args[0]);
+					if (port<1024 || port> 49152){
+						System.out.println("Número de puerto incorrecto. Debe estar entre 1024 y 49152. Elegido el puerto 3000 en su lugar.");
+						port = 3000;
+					}
+				}
+				catch (NumberFormatException e) {
+					System.out.println("Formato de puerto incorrecto. Elegido el puerto 3000 en su lugar.");
+					port = 3000;
+				}
+			}
+			else {
+				port = 3000;
+			}
+
+			ServerSocket wellcomeSocket = new ServerSocket(port);
+
+			while (true)
+			{
+				//Aceptar la nueva petición y crear el SocketManager para gestionar el Socket obtenido
+				SocketManager sm = new SocketManager(wellcomeSocket.accept());
+				System.out.println("Esperando peticiones en el puerto " + port + ".");
+
+		
+				//Crear un objeto HttpRequest para gestionar la petici�n
+				ClientRequest request = new ClientRequest(sm);
+				//Crear un Thread para el objeto ClientRequest
+				Thread hilo = new Thread(request);
+			
+				//Arrancar el Thread
+				hilo.run();
+			}
+		}
+
+	}
+
+}
