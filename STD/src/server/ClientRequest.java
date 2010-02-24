bb20a65a3acadf715f3d35a6f356567137cb89ef
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-02-24 17:38:09 +0000

    Añadidos estados 1 y 2 en ClientRequest

--- STD/src/server/ClientRequest.java
+++ STD/src/server/ClientRequest.java
@@ -3,6 +3,11 @@
  */
 package server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import util.SocketManager;
 
 /**
@@ -12,6 +17,7 @@ import util.SocketManager;
 public class ClientRequest implements Runnable {
 
 	final static String CRLF = "\r\n";
+	String usuario;
 	SocketManager sockManager;
 	
 	public ClientRequest(SocketManager sm){
@@ -23,8 +29,70 @@ public class ClientRequest implements Runnable {
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try{
+			String[] comando;
+			int estado = 0;
+		    while (estado<4){
+				comando = commandParser(sockManager.Leer());
+				
+				switch(estado){
+				case 0:
+					if(comando[0].toUpperCase().equals("USER")){
+						try{
+							usuario = comando[1];
+							sockManager.Escribir("201 OK Bienvenido " + usuario + CRLF);
+							estado = 2;
+						}
+						catch(ArrayIndexOutOfBoundsException e)
+						{// No ha introducido el nombre de usuario.
+							sockManager.Escribir("401 ERR Falta el nombre de usuario" + CRLF);
+						}						
+					}
+					else if (comando[0].toUpperCase().equals("SALIR")){
+						estado = 4;
+					}
+					else {
+						sockManager.Escribir("450 ERR Comando no válido" + CRLF);
+					}
+					break;
+				case 1:
+					if(comando[0].toUpperCase().equals("PASS")){
+						try{
+							if (usuario.equals("user") && comando[1].equals("pass")){
+								sockManager.Escribir("202 OK Bienvenido al sistema" + CRLF);
+								estado = 3;			
+							}
+							else sockManager.Escribir("402 ERR La clave es incorrecta" + CRLF);
+						}
+						catch(ArrayIndexOutOfBoundsException e)
+						{// No ha introducido la clave
+							sockManager.Escribir("403 ERR Falta la clave" + CRLF);
+						}	
+					}
+					else if (comando[0].toUpperCase().equals("SALIR")){
+						estado = 4;
+					}
+					else {
+						sockManager.Escribir("450 ERR Comando no válido" + CRLF);
+					}
+					break;	
+				}
+				sockManager.Escribir("208 OK Salir" + CRLF);
+				sockManager.CerrarSocket();
+				sockManager.CerrarStreams();
+			}
+		}
+		catch (IOException e){
+		      System.err.println(e);
+		}
+	}
 
+
+	
+	
+	private String[] commandParser(String s){
+		String[] comando = s.split(" ");
+		return comando;
 	}
 
 }
