db0a31da694fa7ffc5db027dced63a8d3aabf16a
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-02-25 09:42:20 +0000

    consolaCliente (la del debugueo cutre) acepta la ip y el puerto como parámetro.

--- STD/src/debugueo/consolaCliente.java
+++ STD/src/debugueo/consolaCliente.java
@@ -10,7 +10,32 @@ public final class consolaCliente {
 
 	public static void main(String[] args) {
 		try{
-	        Socket s = new Socket("127.0.0.1",3000);
+			
+			int port;
+			if (args.length>1){//Comprobación de que el puerto es un número válido. 3000 por defecto.
+				try{
+					port = Integer.parseInt(args[1]);
+					if (port<1024 || port> 49152){
+						System.out.println("Número de puerto incorrecto. Debe estar entre 1024 y 49152. Elegido el puerto 3000 en su lugar.");
+						port = 3000;
+					}
+				}
+				catch (NumberFormatException e) {
+					System.out.println("El puerto debe ser un número. Elegido el puerto 3000 en su lugar.");
+					port = 3000;
+				}
+			}
+			else {
+				port = 3000;
+			} 
+			
+			String ip;
+			if (args.length>0)
+				ip = args[0];
+			else ip = "127.0.0.1";
+			
+			
+	        Socket s = new Socket(ip,port);
 	        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 	        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 	        String comando = "";
@@ -27,4 +52,6 @@ public final class consolaCliente {
 			System.out.println(e);
 		}
 	}
+
+	String ip
 }
