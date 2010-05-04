413137571cc550f799c8c1652a5f5d2b8ea3ccdf
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-05-04 21:27:36 +0000

    Prueba de El envío de la foto. creada carpeta img y la imagen.

--- trunk/STD/src/server/command/GetFoto.java
+++ trunk/STD/src/server/command/GetFoto.java
@@ -1,16 +1,43 @@
 package server.command;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import server.ClientRequest;
+import util.SocketManager;
+
 public class GetFoto extends Command
 {
 	@Override
-	public String command(CommandManager commandManager, String[] params)
-	{
+	public String command(ClientRequest cr, String[] params){
 		String respuesta = "";
-		if (commandManager.getState()==2){
-		
+		if (cr.getState()==2){
+			if(cr.isGps()){
+				String fileName = "../img/imagen.jpg";
+				FileInputStream fis  = null;;
+				try {
+					fis = new FileInputStream(fileName);
+					sendBytes(fis,cr.getSockManager());
+					cr.setState(3);
+			    }catch (Exception e) {
+			      System.out.println("No abre fichero");
+			    }
+			}else
+				respuesta = "420 ERR GPS en estado OFF." + CRLF;
 		}else{
 			respuesta = "450 ERR Comando no válido" + CRLF;
 		}
 		return respuesta;
 	}
+	
+	private void sendBytes(FileInputStream fis,SocketManager sm) throws Exception {
+	    // Construct a 1K buffer to hold bytes on their way to the socket.
+	    byte[] buffer = new byte[1024];
+	    int bytes = 0;
+
+	    // Copy requested file into the socket's output stream.
+	    while ( (bytes = fis.read(buffer)) != -1) {
+	      sm.escribir(buffer, bytes);
+	    }
+	  }
 }
