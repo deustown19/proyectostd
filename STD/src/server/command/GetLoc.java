99689532e8df8fe2c56ee5feb97e2cd0babee4b3
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-05-04 21:59:31 +0000

    Getlock finiquitado

--- STD/src/server/command/GetLoc.java
+++ STD/src/server/command/GetLoc.java
@@ -1,6 +1,8 @@
 package server.command;
 
 
+import java.text.DecimalFormat;
+
 import server.ClientRequest;
 
 public class GetLoc extends Command
@@ -10,6 +12,10 @@ public class GetLoc extends Command
 		String respuesta = "";
 		if (cr.getState()==3){
 			respuesta = "" + CRLF;
+			DecimalFormat dec = new DecimalFormat("###.######");
+			double longitud = (Math.random() * 180);
+			double latitud = (Math.random() * 180);
+			respuesta = "114 OK "+dec.format(longitud) + "N-" +  dec.format(latitud) + "E" + CRLF;
 			cr.setState(2);
 		}else{
 			respuesta = "450 ERR Comando no válido" + CRLF;
