2d14f52c6109bfebe1112d5268287ced473c5bcb
orezto <orezto@ace28b22-1c1b-11df-bb07-69a07c54c929>
2010-05-05 00:14:28 +0000

    Añadida funcionalidad extra, si el gps está apagado consultan con el servidor.

--- trunk/STD/src/server/command/GetLoc.java
+++ trunk/STD/src/server/command/GetLoc.java
@@ -4,18 +4,34 @@ package server.command;
 import java.text.DecimalFormat;
 
 import server.ClientRequest;
+import server.GpsAccess;
 
 public class GetLoc extends Command
 {
 	@Override
 	public String command(ClientRequest cr, String[] params){
 		String respuesta = "";
+		String sLongitud = "";
+		String sLatitud = "";
 		if (cr.getState()==3){
-			respuesta = "" + CRLF;
-			DecimalFormat dec = new DecimalFormat("###.######");
-			double longitud = (Math.random() * 180);
-			double latitud = (Math.random() * 180);
-			respuesta = "114 OK "+dec.format(longitud) + "N-" +  dec.format(latitud) + "E" + CRLF;
+			if (cr.isGps()){
+				respuesta = "" + CRLF;
+				DecimalFormat dec = new DecimalFormat("###.######");
+				double longitud = (Math.random() * 180);
+				double latitud = (Math.random() * 180);
+				sLongitud = dec.format(longitud) + "N";
+				sLatitud = dec.format(latitud) + "E";
+			}else{
+				String coordenadas;
+				GpsAccess gps = new GpsAccess();
+				gps.connect("127.0.0.1");
+				gps.user();
+				gps.pass();
+				coordenadas = gps.getCoor("3053");
+				sLongitud = coordenadas.split("-")[0];
+				sLatitud = coordenadas.split("-")[1];
+			}
+			respuesta = "114 OK "+sLongitud + "-" + sLatitud + CRLF;
 			cr.setState(2);
 		}else{
 			respuesta = "450 ERR Comando no válido" + CRLF;
